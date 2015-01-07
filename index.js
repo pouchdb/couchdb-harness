d99f5027a1a39bc49a2d960d72ff0e5904860d09
Marten de Vries <marten94@gmail.com>
2015-01-07 11:06:46 +0100

    (#7) - Updated tests from upstream

--- index.js
+++ index.js
@@ -8,63 +8,78 @@ var spawn   = require('child_process').spawn
   , couchjs = path.resolve(cwd, 'bin/couchjs');
 
 var blacklist = [
-    'script/test/attachments.js',
-    'script/test/attachments_multipart.js',
-    'script/test/attachment_names.js',
-    'script/test/attachment_paths.js',
-    'script/test/attachment_ranges.js',
-    'script/test/auth_cache.js',
-    'script/test/batch_save.js',
-    'script/test/bulk_docs.js',
-    'script/test/coffee.js',
-    'script/test/compact.js',
-    'script/test/config.js',
-    'script/test/conflicts.js',
-    'script/test/cookie_auth.js',
-    'script/test/delayed_commits.js',
-    'script/test/design_docs.js',
-    'script/test/design_options.js',
-    'script/test/erlang_views.js',
-    'script/test/etags_views.js',
-    'script/test/form_submit.js',
-    'script/test/http.js',
-    'script/test/invalid_docids.js',
-    'script/test/jsonp.js',
-    'script/test/list_views.js',
-    'script/test/method_override.js',
-    'script/test/oauth.js',
-    'script/test/oauth_users_db.js',
-    'script/test/proxyauth.js',
-    'script/test/purge.js',
-    'script/test/reader_acl.js',
-    'script/test/recreate_doc.js',
-    'script/test/reduce.js',
-    'script/test/reduce_builtin.js',
-    'script/test/replication.js',
-    'script/test/replicator_db.js',
-    'script/test/replicator_db_security.js',
-    'script/test/rev_stemming.js',
-    'script/test/rewrite.js',
-    'script/test/security_validation.js',
-    'script/test/show_documents.js',
-    'script/test/stats.js',
-    'script/test/update_documents.js',
-    'script/test/users_db.js',
-    'script/test/users_db_security.js',
-    'script/test/uuids.js',
-    'script/test/view_collation.js',
-    'script/test/view_collation_raw.js',
-    'script/test/view_compaction.js',
-    'script/test/view_errors.js',
-    'script/test/view_include_docs.js',
-    'script/test/view_multi_key_all_docs.js',
-    'script/test/view_multi_key_design.js',
-    'script/test/view_multi_key_temp.js',
-    'script/test/view_offsets.js',
-    'script/test/view_pagination.js',
-    'script/test/view_sandboxing.js',
-    'script/test/view_update_seq.js',
-    'script/test/view_xml.js'
+    'script/tests/attachments.js',
+    'script/tests/attachments_multipart.js',
+    'script/tests/attachment_names.js',
+    'script/tests/attachment_paths.js',
+    'script/tests/attachment_ranges.js',
+    'script/tests/auth_cache.js',
+    'script/tests/batch_save.js',
+    'script/tests/bulk_docs.js',
+    'script/tests/coffee.js',
+    'script/tests/compact.js',
+    'script/tests/config.js',
+    'script/tests/conflicts.js',
+    'script/tests/cookie_auth.js',
+    'script/tests/delayed_commits.js',
+    'script/tests/design_docs.js',
+    'script/tests/design_options.js',
+    'script/tests/erlang_views.js',
+    'script/tests/etags_views.js',
+    'script/tests/form_submit.js',
+    'script/tests/http.js',
+    'script/tests/invalid_docids.js',
+    'script/tests/jsonp.js',
+    'script/tests/list_views.js',
+    'script/tests/method_override.js',
+    'script/tests/oauth.js',
+    'script/tests/oauth_users_db.js',
+    'script/tests/proxyauth.js',
+    'script/tests/purge.js',
+    'script/tests/reader_acl.js',
+    'script/tests/recreate_doc.js',
+    'script/tests/reduce.js',
+    'script/tests/reduce_builtin.js',
+    'script/tests/replication.js',
+    'script/tests/replicator_db_bad_rep_id.js',
+    'script/tests/replicator_db_by_doc_id.js',
+    'script/tests/replicator_db_compact_rep_db.js',
+    'script/tests/replicator_db_continuous.js',
+    'script/tests/replicator_db_credential_delegation.js',
+    'script/tests/replicator_db_field_validation.js',
+    'script/tests/replicator_db_filtered.js',
+    'script/tests/replicator_db_identical.js',
+    'script/tests/replicator_db_identical_continuous.js',
+    'script/tests/replicator_db_invalid_filter.js',
+    'script/tests/replicator_db_simple.js',
+    'script/tests/replicator_db_successive.js',
+    'script/tests/replicator_db_survives.js',
+    'script/tests/replicator_db_swap_rep_db.js',
+    'script/tests/replicator_db_update_security.js',
+    'script/tests/replicator_db_user_ctx.js',
+    'script/tests/replicator_db_write_auth.js',
+    'script/tests/rev_stemming.js',
+    'script/tests/rewrite.js',
+    'script/tests/security_validation.js',
+    'script/tests/show_documents.js',
+    'script/tests/stats.js',
+    'script/tests/update_documents.js',
+    'script/tests/users_db.js',
+    'script/tests/users_db_security.js',
+    'script/tests/uuids.js',
+    'script/tests/view_collation.js',
+    'script/tests/view_collation_raw.js',
+    'script/tests/view_compaction.js',
+    'script/tests/view_errors.js',
+    'script/tests/view_include_docs.js',
+    'script/tests/view_multi_key_all_docs.js',
+    'script/tests/view_multi_key_design.js',
+    'script/tests/view_multi_key_temp.js',
+    'script/tests/view_offsets.js',
+    'script/tests/view_pagination.js',
+    'script/tests/view_sandboxing.js',
+    'script/tests/view_update_seq.js',
+    'script/tests/view_xml.js'
   ];
 
 module.exports = {
@@ -78,27 +93,28 @@ module.exports = {
 
     tests = (!!tests && tests.length)
       ? tests.map(function (test) { 
-          return 'script/test/' + test + '.js';
+          return 'script/tests/' + test + '.js';
         })
-      : glob.sync('script/test/*.js', { cwd: cwd });
+      : glob.sync('script/tests/*.js', { cwd: cwd });
 
     tests = tests.filter(function(test) {
       return blacklist.indexOf(test) === -1;
     });
 
-    console.log('testing: %s', tests.join('\n'));
-    console.log('skipping: %s', blacklist.join('\n'));
+    console.log('testing:\n%s\n', tests.join('\n'));
+    console.log('skipping:\n%s\n', blacklist.join('\n'));
 
     files = [
       'script/json2.js',
       'script/sha1.js',
       'script/oauth.js',
       'script/couch.js',
+      'script/replicator_db_inc.js',
       'script/couch_test_runner.js',
-      'script/couch_tests.js'
+      'script/couch_http.js',
+      'script/test_setup.js'
     ].concat(tests, [
-      'util/couch_http.js',
-      'util/cli_runner.js'
+      'script/cli_runner.js'
     ]).map(function (fp) {
       return path.resolve(cwd, fp);
     });
